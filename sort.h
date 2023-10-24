97d45f267ca8488fc655028b58236eafe8c66245
Adams2005 <chrisadamsadewuni@gmail.com>
2023-10-24 05:21:06 +0100

    Shell sort - Knuth Sequence

--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ void quick_sort(int *array, size_t size);
 void sorter(int *array, int low, int high, size_t size);
 int partition(int *array, int low, int high, size_t size);
 void swap_node(listint_t **list, listint_t **left, listint_t *right);
+void shell_sort(int *array, size_t size);
 
 #endif
